324a72b954e3a112129ec24712bf4a27a0ebc72a
metiinie <abumahilkerim@gmail.com>
2026-08-29 21:30:57 +0300

    enhance user page

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { PrismaModule } from './prisma/prisma.module';
